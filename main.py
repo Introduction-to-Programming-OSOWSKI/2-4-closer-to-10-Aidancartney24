e2c34eb819952a3911bb1e6b1514aa120f155b31
Aidancartney24 <824776@mystma.org>
2021-09-28 15:18:54 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    
+    abs(10 - x)
+
+    abs(10 - y)
+
+    if x > y:
+        return x
+
+    elif x == y:
+        return "0"
+
+    else:
+        return y
+        
+print(close10(8, 13))
